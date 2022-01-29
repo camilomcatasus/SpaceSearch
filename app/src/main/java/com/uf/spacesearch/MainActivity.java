8ef6d684d93c1026e4e4e26599cfa73981e179df
Camilo Catasus <camilomcatasus@gmail.com>
2022-01-28 22:28:16 -0500

    Basic Menu Context Switching

--- app/src/main/java/com/uf/spacesearch/MainActivity.java
+++ app/src/main/java/com/uf/spacesearch/MainActivity.java
@@ -2,20 +2,28 @@ package com.uf.spacesearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.uf.spacesearch.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+        button = findViewById(R.id.Play);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PlayGame();
+            }
+        }) ;
+    }
+    private void PlayGame() {
+        Intent playGameIntent = new Intent(this, GameActivity.class);
+        startActivity(playGameIntent);
     }
 }
